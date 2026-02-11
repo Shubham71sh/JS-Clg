41646d0f019a59ab01252a9498526a5f388a4ba6
Shubham71sh <shubhuchandel71@gmail.com>
2026-02-11 15:27:42 +0530

    js

---
+++ 6th/Array/script.js
@@ -0,0 +1,31 @@
+//            Array
+
+// const num = [3,5,4,8,2,1];
+// console.log(num[1]);
+
+
+// const getValue = (arr, idx) => {
+//     return arr[idx];
+// };
+// const numbers = [10, 20, 30, 40];
+
+// console.log(getValue(numbers, 0));
+
+// console.log(numbers.length);
+
+// const arr = [10, 20, 30, 40,50,60,70,80,90,100];
+
+// arr.forEach(function(val){
+//     console.log(val);
+// });
+
+const arri = [10, 20, 30, 40,50,60,70,80,90,100];
+let i =0;
+do{
+    {
+        console.log(arri[i]);
+    i++;
+    }
+}
+while(i < arri.length);
+
